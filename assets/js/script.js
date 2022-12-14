b09f065534d40a320dce1418371f6980a351fe21
Freedy-FR <freedy.ramos@hotmail.com>
2022-12-14 22:52:22 +0000

    feat: :sparkles: Add start game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,37 @@
 // Variables
 let dataArray;
 
+let startButton = document.getElementById("btn-start");
+
+let nameInput = document.getElementById("name");
+
+let mainPage = document.getElementById("main-page");
+let gameArea = document.getElementById("game-area");
+
 // Api
-let url = "https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple"
+let url = "https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple";
 
 
 /**
  * Api function to fetch an array of objects with questions and answer
  */
 async function getData() {
-    let response = await fetch(url)
-    let data = await response.json()
+    let response = await fetch(url);
+    let data = await response.json();
     return data;
-}
+};
+
+// Start game function
+
+startButton.addEventListener("click", startGame);
+
+
+function startGame() {
+    // startButton.classList.add("hide");
+    if (nameInput.value == "") {
+        alert("Please insert a name to play!");
+    } else {
+        mainPage.classList.add("hide");
+        gameArea.classList.remove("hide");
+    }
+};
