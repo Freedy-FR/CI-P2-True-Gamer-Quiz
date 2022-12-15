8f606bc4a532b22982f982b2dd0f12db7ccd708f
Freedy-FR <freedy.ramos@hotmail.com>
2022-12-15 01:47:59 +0000

    feat: :sparkles: Add DOM loaded event listener and reorganized the code position

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,29 @@
 // Variables
-let dataArray;
-
 let startButton = document.getElementById("btn-start");
 
 let nameInput = document.getElementById("name");
 let questionInput = document.getElementById("question");
 let answerGrid = document.getElementById("btn-answer");
+
 let correctAnswer = "";
 let incorrectAnswer = "";
+
+let currentScore = 0;
 let numberOfQuestions = 10;
 
 let mainPage = document.getElementById("main-page");
 let gameArea = document.getElementById("game-area");
 
-// Api
-let url = "https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple";
+// DOM load
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("scoreCount").innerHTML = currentScore;
+    document.getElementById("total-questions").innerHTML = numberOfQuestions;
+    getData();
+});
 
+
+// Api url
+let url = "https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple";
 
 /**
  * Api function to fetch an array of objects with questions and answer
@@ -23,27 +31,11 @@ let url = "https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&typ
 async function getData() {
     let response = await fetch(url);
     let data = await response.json();
-    // console.log(data.results);
-    // return data;
-    console.log(data.results[0]);
     showQuestion(data.results[0]);
 };
 
-// Start game function
 
-startButton.addEventListener("click", startGame);
-
-
-function startGame() {
-    // startButton.classList.add("hide");
-    if (nameInput.value == "") {
-        alert("Please insert a name to play!");
-    } else {
-        mainPage.classList.add("hide");
-        gameArea.classList.remove("hide");
-    }
-};
-
+// Show Questions and answers function
 function showQuestion(data) {
     correctAnswer = data.correct_answer;
     incorrectAnswer = data.incorrect_answers;
@@ -57,4 +49,15 @@ function showQuestion(data) {
     `;
 }
 
-getData();
+
+// Start game function
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    if (nameInput.value == "") {
+        alert("Please insert a name to play!");
+    } else {
+        mainPage.classList.add("hide");
+        gameArea.classList.remove("hide");
+    }
+};
